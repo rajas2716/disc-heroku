4b6edb2765dd220c6acb0c9c96f73a7c3c87d66c
rajas2716 <rajaspaunikar2716@gmail.com>
2021-11-20 14:43:36 +0530

    fd

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ while 1:
     }
 
     header = {
-    'authorization' : 'MzY3MjY5MTc4NzA2NjI0NTEy.YThNoQ._gbENH1x7G7_cerEgc5601-Bek4'
+    'authorization' : 'MzY3MjY5MTc4NzA2NjI0NTEy.YZi7NQ.q0IhwFmygmfj4UHdtEVrxmMDXrk'
     }
 
     r = requests.post("https://discord.com/api/v9/channels/892453734301433936/messages", data = payload, headers = header) 
